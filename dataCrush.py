7ad726854acc452f0686e32322cecdc60afe8602
demos <gdemosg@gmail.com>
2017-04-07 10:35:58 +0200

    1 - First commit.

--- dataCrush.py
+++ dataCrush.py
@@ -1,4 +1,1 @@
-import pandas as pd
 import numpy as np
-import scipy as sp
-import matplotlib.pyplot as plt
---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='deloitte_hack',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='gdemos',
+    author_email='',
+    description=''
+)
