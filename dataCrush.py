3745bb68d4dc6fd8ea5946ee11c3b4701b0f6f7f
demos <gdemosg@gmail.com>
2017-04-08 09:55:53 +0200

    1 - First commit. 2 - Further functions; no notebooks 2.1 - added Notebooks 3 - Final version 3.1 - More Polished Final Version
    NB! ->>> Check notebook and change 'path' where data files are located before starting.

--- dataCrush.py
+++ dataCrush.py
@@ -1,7 +1,11 @@
+import dataCrush as dc
 import pandas as pd
 import numpy as np
 import scipy as sp
 import matplotlib.pyplot as plt
+from os import listdir
+import plotly as pl
+from sklearn.ensemble import RandomForestRegressor
 
 import matplotlib as mp
 font = {'family' : 'Helvetica',
@@ -100,6 +104,13 @@ def computeEfectvinessRatioPoliciesPerRegion(Region='WEST AFRICA'):
 
 ############################################
 def CoverageRatePerVaccine(year='2015'):
+
+    """
+    -> Grouping by region and showing all vaccines. <-
+    :param year: Define a year for computing the mean
+    :return: Dataframe with the mean values of the coverage rate
+    """
+
     # Select a dataset
     val = 'WHO_Coverage_estimates.csv'
     data = pd.read_csv(path + val)
@@ -111,9 +122,146 @@ def CoverageRatePerVaccine(year='2015'):
         res = getFeaturesFromVacineTrype(data, vaccine=vcns[i])
         r = res.groupby('Region').mean();
         r = r[r.columns[1:]]
-        r = pd.DataFrame(r['2015']).T
+        r = pd.DataFrame(r[year]).T
         R = pd.concat([R, r], axis=0)
 
     R.index = vcns
 
     return R
+
+
+############################################
+def printDiseasePerCountry():
+
+    """
+    Print the top diseases per region:
+    ----------------------------------
+        - Idea is to see the top expenditure per disease
+          per region.
+    """
+    # all regions within the dataset
+    regios = ['EMRO', 'EURO', 'CENTRAL AFRICA',
+              'SEARO', 'WEST AFRICA', 'PAHO',
+              'WPRO', 'SOUTH AFRICA', 'EAST AFRICA']
+
+    # create DF
+    DF = pd.DataFrame()
+
+    # Iterate over regions
+    for reg in regios:
+        df = computeEfectvinessRatioPoliciesPerRegion(Region=reg)
+        DF = pd.concat([DF, df], axis=1)
+
+    # Printing func
+    print('-> Top expenditure per desease per region <-')
+    print('----')
+    for i in range(len(DF.columns)):
+        dis = pd.DataFrame(DF[DF.columns[i]].sort_values(ascending=False)).index[0]
+        val = pd.DataFrame(pd.DataFrame(DF[DF.columns[i]].sort_values(ascending=False)).ix[1]).values[0]
+        print('(disease -> %s)  --- (Region -> %s) --- (expenditure = %.1f USD Mi.)' % (dis, DF.columns[i], val))
+
+
+############################################
+def plotMeanDisbusementPerRegion(savePdf=False):
+
+    #import portfolio_functions as pf
+    # To be used on my pc only for normalizing data
+
+    data2 = pd.read_csv(path + 'NGO_DataDisbursement.csv')
+
+    gd = data2.groupby('Region').mean().T
+    gd = gd.ix[gd.index[1:]]
+
+    #gd = pf.normalize_data_for_comparacy(gd, sklearn=False)
+
+    f, ax = plt.subplots(1, 1, figsize=(6, 4))
+    gd.plot(ax=ax, marker='o', linewidth=.5, alpha=0.9)
+    plt.title(r'$Mean\, Disbursement\, per\, region$', fontsize=18)
+    plt.ylabel('$Disbursement\, in\, USD\, Bi.$', fontsize=18)
+    plt.legend(fancybox=True, framealpha=0.4, loc='upper left',
+               fontsize=9)
+    plt.grid(linewidth=.3)
+    plt.tight_layout()
+
+    if savePdf == False:
+        pass
+    else:
+        plt.savefig('/Users/Demos/Desktop/cnt.pdf')
+
+    return gd
+
+############################################
+def getRankedCountriesForGivenVaccine(data, vaccine='DTP3'):
+    """
+    Get all other features for a given vaccine.
+    """
+    df = getFeaturesFromVacineTrype(data, vaccine=vaccine)
+    return df
+
+
+############################################
+def computeAndPlotAggCoveragePerRegion(year='2015', vaccine='DTP3'):
+    # Select a dataset
+    val = 'WHO_Coverage_estimates.csv'
+    data = pd.read_csv(path + val)
+
+    val2 = 'Death rate, crude (per 1,000 people).xls'
+    data2 = pd.read_excel(path + val2)
+
+    # PER REGION
+    df = getRankedCountriesForGivenVaccine(data, vaccine=vaccine)
+    aggData = df.groupby('Region').mean().T
+    aggData = aggData.ix[aggData.index[1:-1]][::-1]
+
+    f, ax = plt.subplots(1, 1, figsize=(6, 4))
+    df[df.columns[5:-1]].T[::-1].mean(axis=1).plot(kind='area', color='k',
+                                                   alpha=0.3, ax=ax)
+    df[df.columns[5:-1]].T[::-1].mean(axis=1).plot(color='k', marker='o', ax=ax,
+                                                   alpha=0.5)
+    aggData.plot(ax=ax)
+    ax.grid(True)
+    plt.title(r'$Mean(Aggregated$ $Coverage$ $Rate)$ $in$ $P.P.$', fontsize=18)
+    plt.ylabel('$Percentage\, of\, coverage (\%)$', fontsize=18)
+    plt.ylim([0, 100])
+    plt.tight_layout()
+
+    return aggData
+
+
+############################################
+def predictCovRatioForAfricanCountries(gd, aggData):
+
+    """
+    :param gd:
+    :param aggData:
+    :return: Predicting future trends of coverage ratio and
+    """
+
+    # DATA MUNGE: Selecting training and testing data
+    x_train = gd[['WEST AFRICA', 'EAST AFRICA', 'SOUTH AFRICA', 'CENTRAL AFRICA']]['2003':'2015']
+    x_train = x_train.values
+    x_test = gd[['WEST AFRICA', 'EAST AFRICA', 'SOUTH AFRICA', 'CENTRAL AFRICA']]['2015':'2017'].values
+    labels = aggData['AFR'][gd['WEST AFRICA'].index]['2003':'2015'].values
+
+    # TRAIN AND PREDICT
+    model2 = RandomForestRegressor()
+    model2.fit(x_train, labels)
+    pred = model2.predict(x_test)
+    newDf = aggData['AFR'][gd['WEST AFRICA'].index]
+    newDf['2016'] = pred[0];
+    newDf['2017'] = pred[1]
+
+    # PLOT
+    # gd[['WEST AFRICA','EAST AFRICA', 'SOUTH AFRICA', 'CENTRAL AFRICA']].plot()
+    gd[['WEST AFRICA', 'EAST AFRICA', 'SOUTH AFRICA', 'CENTRAL AFRICA']].plot()
+    plt.legend(fancybox=True, framealpha=0.2, loc='upper left')
+    plt.ylabel('$Expenditure$ $in$ $USD$ $Bi$', fontsize=16)
+    ax = plt.twinx()
+    newDf.plot(linestyle='--', color='k', ax=ax)
+    newDf['2001':'2015'].plot(linestyle='-', color='k', ax=ax)
+    aggData['AFR'][gd['WEST AFRICA'].index].plot(kind='area', alpha=0.2,
+                                                 ax=ax, color='k')
+    ax.set_ylabel('$Percentage$ $coverage$', fontsize=16)
+    ax.set_ylim([60, 100])
+    plt.tight_layout()
+    # plt.savefig('/Users/demos/Desktop/deloitte /prediction.pdf')
